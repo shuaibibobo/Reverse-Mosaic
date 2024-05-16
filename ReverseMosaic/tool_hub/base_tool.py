f74114ec6c2a1fe2bd4acdc139185c9aa2cb99a6
James Stevenson <28961606+user1342@users.noreply.github.com>
2024-05-16 18:51:12 +0100

    Update base_tool.py

--- ReverseMosaic/tool_hub/base_tool.py
+++ ReverseMosaic/tool_hub/base_tool.py
@@ -1,8 +1,26 @@
+from llama_index.core.tools import FunctionTool
 
 class BaseToolClass:
 
-    def return_tools(self)->list:
+    def get_tool_functions()-> list:
+        """Returns a list of functions that can be run as tools by the multi agent"""
         raise Exception("Not overriden exception")
+        return []
+
+    def return_tools(self) -> list:
+        """Returns a list of FunctionTool instances for Ghidra-related functions.
+
+        Returns:
+            list: A list of FunctionTool instances.
+        """
+
+        list_of_functions = self.get_tool_functions()
+        tools = []
+        for fun in list_of_functions:
+            tool = FunctionTool.from_defaults(fun,)
+            tools.append(tool)
+
+        return tools
     
 
 def get_class():
